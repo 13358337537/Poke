7ec207373b7c7bc5789507233320fae4810771d7
cmk <761438054@qq.com>
2020-12-17 17:46:02 +0800

    12.17 第一次提交(集成依赖)

---
+++ buildSrc/src/main/java/com/cmk/buildsrc/BuildConfig.kt
@@ -0,0 +1,10 @@
+package com.cmk.buildsrc
+
+object BuildConfig {
+    const val compileSdkVersion = 30
+    const val buildToolsVersion = "29.0.3"
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 30
+    const val versionCode = 10001
+    const val versionName = "1.0.1"
+}
---
+++ buildSrc/src/main/java/com/cmk/buildsrc/Dependencies.kt
@@ -0,0 +1,86 @@
+package com.cmk.buildsrc
+
+object Versions {
+    // gradle plugins
+    const val gradleBuildTool = "4.1.1"
+    const val spotlessGradle = "5.8.2"
+    const val versionPlugin = "0.28.0"
+
+    // kotlin
+    const val kotlin = "1.4.21"
+
+    // support library
+    const val materialVersion = "1.2.0-alpha06"
+    const val constraintVersion = "2.0.2"
+
+    // architecture components
+    const val fragmentVersion = "1.2.5"
+    const val lifecycleVersion = "2.2.0"
+    const val roomVersion = "2.3.0-alpha03"
+    const val archComponentVersion = "2.1.0"
+
+    // startup
+    const val startupVersion = "1.0.0"
+
+    // di
+    const val hiltCoreVersion = "2.30.1-alpha"
+    const val hiltVersion = "1.0.0-alpha02"
+    const val assistedInjectVersion = "0.5.2"
+
+    // network
+    const val retrofitVersion = "2.9.0"
+    const val okhttpVersion = "4.9.0"
+    const val sandwichVersion = "1.0.6"
+
+    // moshi
+    const val moshiVersion = "1.11.0"
+    const val moshiConverterVersion = "2.7.2"
+
+    // coroutines
+    const val coroutinesVersion = "1.4.1"
+
+    // whatIf
+    const val whatIfVersion = "1.0.9"
+
+    // glide
+    const val glideVersion = "4.11.0"
+    const val glidePaletteVersion = "2.1.2"
+
+    // bundler
+    const val bundlerVersion = "1.0.3"
+
+    // transformation
+    const val transformationLayout = "1.0.4"
+
+    // adapter
+    const val recyclerView = "1.2.0-alpha06"
+    const val baseAdapter = "0.1.3"
+
+    // android ribbon
+    const val androidRibbonVersion = "1.0.3"
+
+    // progress view
+    const val progressViewVersion = "1.1.0"
+
+    // rainbow
+    const val rainbowVersion = "1.0.3"
+
+    // debugging
+    const val timberVersion = "4.7.1"
+
+    // unit test
+    const val truthVersion = "1.0.1"
+    const val junitVersion = "4.13.1"
+    const val turbineVersion = "0.2.0"
+    const val robolectricVersion = "4.4"
+    const val androidxTest = "1.3.0"
+    const val androidxTestJunit = "1.1.2"
+    const val androidTestRunner = "1.3.0-beta01"
+    const val espressoVersion = "3.3.0"
+    const val mockitoKotlinVersion = "2.2.0"
+    const val mockitoInlineVersion = "3.5.13"
+}
+
+object Dependencies {
+
+}
